d3d4e9c28e0e5a3c5359e62b2deea4f0b8dcfe68
And-now-I-use-GitHub <140209237+And-now-I-use-GitHub@users.noreply.github.com>
2023-07-21 21:43:14 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,10 +1,10 @@
 // create web server
-// Path: server.js
-// var http = require('http');
-// var server = http.createServer(function(req, res) {
-//     res.writeHead(200, {
-//         'Content-Type': 'text/plain'
-//     });
-//     res.end('Hey ninjas');
-// });
-// server.listen(3000, '
+ Path: server.js
+ var http = require('http');
+ var server = http.createServer(function(req, res) {
+     res.writeHead(200, {
+         'Content-Type': 'text/plain'
+     });
+    res.end('Hey ninjas');
+});
+server.listen(3000)
