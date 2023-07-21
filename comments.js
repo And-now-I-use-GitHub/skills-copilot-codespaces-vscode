1f162c1da54f51023426e661149f16f6a0f4503e
And-now-I-use-GitHub <140209237+And-now-I-use-GitHub@users.noreply.github.com>
2023-07-21 21:56:18 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,70 +1,51 @@
-// create web server
-// create a web server using http module
-var http = require('http');
-// create a web server using http module
-var server = http.createServer(function (req, res) {
-    // set the response header
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    // set the response content
-    res.write('<html><body><p>This is home Page.</p></body></html>');
-    res.end();
-});
-// listen to the port
-server.listen(5000);
-console.log('Node.js web server at port 5000 is running..');
-// create web server
-// create a web server using express module
-var express = require('express');
-// create express app
-var app = express();
-// setup the server port
-var port = process.env.PORT || 5000;
-// set the route
-app.get('/', function (req, res) {
-    // set the response
-    res.send('Hello World with Express');
-});
-// listen to the port
-app.listen(port, function () { return console.log("Listening on port " + port); });
-// create web server
-// create a web server using express module
-var express = require('express');
-// create express app
-var app = express();
-// setup the server port
-var port = process.env.PORT || 5000;
-// set the route
-app.get('/', function (req, res) {
-    // set the response
-    res.send('Hello World with Express');
-});
-// listen to the port
-app.listen(port, function () { return console.log("Listening on port " + port); });
-// create web server
-// create a web server using express module
-var express = require('express');
-// create express app
-var app = express();
-// setup the server port
-var port = process.env.PORT || 5000;
-// set the route
-app.get('/', function (req, res) {
-    // set the response
-    res.send('Hello World with Express');
-});
-// listen to the port
-app.listen(port, function () { return console.log("Listening on port " + port); });
-// create web server
-// create a web server using express module
-var express = require('express');
-// create express app
-var app = express();
-// setup the server port
-var port = process.env.PORT || 5000;
-// set the route
-app.get('/', function (req, res) {
-    // set the  response
-    res.send('Hello World with Express');
-}
-// listen to the port
-app.listen(port, function () { return console.log("Listening on port " + port); });
+// Create web server
+// Create a folder named nodejs
+// Create a file named server.js
+// Initialize npm
+// npm init
+// Install express
+// npm install express --save
+// Create a folder named public
+// Create a folder named views
+// Create a file named index.html in the public folder
+// Create a file named index.js in the views folder
+// Create a file named style.css in the public folder
+// Create a file named about.html in the public folder
+// Create a file named about.js in the views folder
+// Create a file named about.css in the public folder
+// Create a file named contact.html in the public folder
+// Create a file named contact.js in the views folder
+// Create a file named contact.css in the public folder
+// Create a file named services.html in the public folder
+// Create a file named services.js in the views folder
+// Create a file named services.css in the public folder
+// Create a file named skills.html in the public folder
+// Create a file named skills.js in the views folder
+// Create a file named skills.css in the public folder
+// Create a file named member.html in the public folder
+// Create a file named member.js in the views folder
+// Create a file named member.css in the public folder
+// Create a file named comments.html in the public folder
+// Create a file named comments.js in the views folder
+// Create a file named comments.css in the public folder
+// Create a file named server.js
+// Create a file named .gitignore
+// Create a file named package.json
+// Create a file named package-lock.json
+// Create a file named README.md
+// Create a file named LICENSE
+// Create a file named .env
+// Create a file named .travis.yml
+// Create a file named .appveyor.yml
+// Create a file named .eslintrc.json
+// Create a file named .eslintignore
+// Create a file named .babelrc
+// Create a file named .gitattributes
+// Create a file named .editorconfig
+// Create a file named .prettierrc
+// Create a file named .npmignore
+// Create a file named .codeclimate.yml
+// Create a file named .coveralls.yml
+// Create a file named .all-contributorsrc
+// Create a file named .github/ISSUE_TEMPLATE/config.yml
+// Create a file named .github/
